a5130de40ee9f2f3bb3eb258fe78ce8aa528c36d
salihbout <salihb@qq.com>
2020-07-24 02:33:34 +0300

    dockerfile added, model download script edited

--- bin/download_model.py
+++ bin/download_model.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
 import gdown
 
+print('Downloading trained BERT model ...')
 gdown.download(
     "https://drive.google.com/uc?id=1CDMDr12Ce0FV3ipHXrWvqg3bjxZZnsLY",
     "assets/model_state_dict.bin",
 )
+print('BERT model downloaded')
