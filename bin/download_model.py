0d22c1809bfa43674bfbeefdfc378990cf704196
salihbout <salihb@qq.com>
2020-07-28 02:17:27 +0300

    assets folder build during deploy

--- bin/download_model.py
+++ bin/download_model.py
@@ -4,6 +4,6 @@ import gdown
 print('Downloading trained BERT model ...')
 gdown.download(
     "https://drive.google.com/uc?id=1CDMDr12Ce0FV3ipHXrWvqg3bjxZZnsLY",
-    "assets/model_state_dict.bin",
+    "assets/bert_model_state.bin",
 )
 print('BERT model downloaded')
