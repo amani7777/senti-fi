ccb576b34aa7696ebae206ee620c8ee2089edf93
salihbout <salihb@qq.com>
2020-08-04 23:20:31 +0300

    added twitter search type selection, new gif

--- sentifi/app.py
+++ sentifi/app.py
@@ -5,7 +5,8 @@ from classifier.model import  get_model
 from PIL import Image
 import json
 from dateutil.parser import parse
-
+import plotly.express as px
+from geopy.geocoders import Nominatim
 
 model = get_model()
 
@@ -29,7 +30,7 @@ if __name__ == "__main__":
 
     option = st.sidebar.radio(
             'How would you like to perform sentiment analysis ?',
-            ('Let me write some text', 'Tweets')
+            ('Let me write some text', 'Get some Tweets')
         )
 
     EMOJIS_MAP = {
@@ -38,7 +39,7 @@ if __name__ == "__main__":
             'negative' : ':rage:'
         }
 
-    if option != 'Tweets' :
+    if option != 'Get some Tweets' :
         txt = st.text_area('Text to analyze', '''Oikein hyvä''')
 
         if st.button('Analyze'):
@@ -55,33 +56,54 @@ if __name__ == "__main__":
         python_tweets = Twython(creds['CONSUMER_KEY'], creds['CONSUMER_SECRET'])
         twitter_keywords =  st.text_input('Twitter keywords')
         number_tweets =  st.number_input('Number of Tweets')
+        result_type = st.radio(
+            'Result type',
+            ('Recent', 'Popular')
+        )
         fetch_tweets = st.button('Go')
-        percent_complete = 0
-        my_bar = st.progress(percent_complete)
+
         if fetch_tweets :
             query = {'q': twitter_keywords,
-                    'result_type' : 'recent',
+                    'result_type' : result_type,
+                    'count':number_tweets,
                     'lang': 'fi',
                     }
-  
-
+            percent_complete = 0
+            my_bar = st.progress(percent_complete)
             with st.spinner('Fetching tweets...'): 
-                dict_ = { 'date': [], 'text': [], 'sentiment': [], 'confidence' : []}
+                dict_ = { 'date': [], 'text': [], 'location':[], 'sentiment': [], 'confidence' : [], 'latitude':[], 'longitude':[]}
+                geolocator = Nominatim(user_agent="sentifi")
+
                 for status in python_tweets.search(**query)['statuses']:
-                    date = parse(status['created_at'])
-                    text = status['text']
-                    dict_['date'].append(date)
-                    dict_['text'].append(text)
-                    results = predict(text)
+                    percent_complete +=1
+                    my_bar.progress(int((percent_complete / number_tweets)*100))
+                    dict_['date'].append(parse(status['created_at']))
+                    dict_['text'].append(status['text'])
+                    dict_['location'].append(status['user']['location'].lower())
+                    print('getting coords')
+                    location = geolocator.geocode(status['user']['location'].lower())
+                    print(location)
+                    if location:
+                        dict_['latitude'].append(location.latitude)
+                        dict_['longitude'].append(location.longitude)
+                    else :
+                        dict_['latitude'].append(0)
+                        dict_['longitude'].append(0)
+           
+
+                    results = predict(status['text'])
                     dict_['sentiment'].append(results["sentiment"])
-                    dict_['confidence'].append(int(results["confidence"]*100))
+                    dict_['confidence'].append(results["confidence"])
                     
-                    st.markdown(text)
-                    st.markdown(f'{date}')
-                    st.markdown(f'{EMOJIS_MAP[results["sentiment"]]} {results["sentiment"]} with a confidence of {int(results["confidence"]*100)}%')
-                    st.markdown('***')
                 df = pd.DataFrame(dict_)
-            st.dataframe(df)    
-            st.sidebar.markdown("***")        
-            st.sidebar.text(f'Total Tweets: {len(df)}')
-            st.sidebar.text(df.sentiment.value_counts().to_dict())
+            st.markdown("***")
+            st.text(f'Sentiment Analysis Summary')
+            fig = px.pie(df, names='sentiment', color_discrete_map={'negative':'red', 'positive':'green', 'neutral':'blue'})
+            st.write(fig) 
+            st.map(df)
+            for idx, row in df.iterrows():
+                st.markdown("***")        
+                st.markdown(row['text'])
+                st.markdown(row['date'])
+                st.markdown(f'{EMOJIS_MAP[row["sentiment"]]} {row["sentiment"]} with a confidence of {int(row["confidence"]*100)}%')
+
